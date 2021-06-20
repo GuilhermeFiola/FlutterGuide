d0daef0ca919fb3060be76acbc33c3eef686a2a0
Guilherme Fiola <guilhermefiola@gmail.com>
2021-06-20 09:53:36 -0300

    Flutter Basics: Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite food?',
+      'What\'s your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First App'),
+        ),
+        body: Column(children: [
+          Text('The question!'),
+          RaisedButton(
+            child: Text('Answer1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer2'),
+            onPressed: () => print('Answer 2 chosen!'),
+          ),
+          RaisedButton(
+            child: Text('Answer3'),
+            onPressed: () {
+              print('Answer 3 chosen!');
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+}
